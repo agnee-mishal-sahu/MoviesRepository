fb47c351477ec259dd8eb4130fe119db1fc29ee1
agnee21 <agneemishal2@gmail.com>
2023-10-19 15:50:21 +0530

    Entity class commit

--- src/main/java/com/cg/Entity/Movies.java
+++ src/main/java/com/cg/Entity/Movies.java
@@ -1,8 +1,24 @@
 package com.cg.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Movies {
 	
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int moviesId;
+	private String movieName;
+	private String genre;
 	
 	
 }
